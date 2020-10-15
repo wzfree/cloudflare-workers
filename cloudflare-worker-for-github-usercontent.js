cb75290c65c503aface75ad4daa0e94e196e57e1
openflake <app@zerg.cc>
2020-10-15 16:19:51 +0800

    Create cloudflare-worker-for-github-usercontent.js

---
+++ cloudflare-worker-for-github-usercontent.js
@@ -0,0 +1,13 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+    const url = new URL(request.url)
+    if (url.pathname === '/') {
+        return await fetch('https://cdn.unpkg.net/index.html')
+    }
+    url.hostname = 'raw.githubusercontent.com'
+    url.pathname = url.pathname.replace(/^\/([^/]+\/[^/]+)\/(.+)$/, "$1/master/$2")
+    return await fetch(new Request(url, request))
+}
