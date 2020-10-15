552b8a31e56dafd28b8f4f993d887d6bc29cfd58
openflake <app@zerg.cc>
2020-10-15 16:23:34 +0800

    simpify

--- cloudflare-worker-for-mirror.js
+++ cloudflare-worker-for-mirror.js
@@ -1,9 +1,6 @@
-const upstreams = {
-    // subdomain: ['desktop website', 'mobile website']
-    google: ['https://www.google.com'],
-    image: ['https://image.google.com'],
-    play: ['https://play.google.com'],
-    wiki: ['https://zh.wikipedia.org', 'https://zh.m.wikipedia.org']
+const upstream = {
+    desktop: 'https://zh.wikipedia.org',
+    mobile: 'https://zh.m.wikipedia.org'
 }
 
 addEventListener('fetch', event => {
@@ -11,42 +8,26 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
+    const userAgent = request.headers.get('user-agent') || ''
+    const upstreamAdapter = isMobile(userAgent) ? upstream.mobile : upstream.desktop
+    const upstreamUrl = new URL(upstreamAdapter)
+
     const requestUrl = new URL(request.url)
-    const subdomain = requestUrl.host.split('.')[0]
-    const upstream = upstreams[subdomain]
-
-    if (!upstream || !upstream[0]) {
-        return new Response(
-            `Bad Request: The upstream '${subdomain}' is not available.`, {
-                status: 400
-            }
-        )
-    }
-
-    const upstreamUrl = isMobile(request) ?
-        new URL(upstream[1] || upstream[0]) :
-        new URL(upstream[0])
-
     requestUrl.protocol = upstreamUrl.protocol
     requestUrl.host = upstreamUrl.host
     requestUrl.pathname = upstreamUrl.pathname + requestUrl.pathname
 
-    return await fetch(
-        new Request(requestUrl, {
-            method: request.method,
-            headers: request.headers,
-            body: request.body
-        })
-    )
+    return await fetch(new Request(requestUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+    }))
 }
 
-function isMobile(request) {
-    const userAgent = request.headers.get('user-agent')
-    if (userAgent) {
-        const mobileAgents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod']
-        for (let agent of mobileAgents) {
-            if (userAgent.indexOf(agent) > 0) return true
-        }
+function isMobile(userAgent) {
+    const mobileAgents = ['Android', 'iPhone', 'SymbianOS', 'Windows Phone', 'iPad', 'iPod']
+    for (let agent of mobileAgents) {
+        if (userAgent.indexOf(agent) > 0) return true
     }
     return false
 }
